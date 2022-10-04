3512555f345f185fd6054a5768b9020004689519
Vlad <vladcristache1996@yahoo.com>
2022-10-04 22:11:48 +0300

    added css and more functionality

--- app.js
+++ app.js
@@ -2,28 +2,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.static("public"));
+
+let items = ["Buy Food", "Coock Food" , "Eat Food"];
+
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", function(req, res) {
 
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options)
+  let day = today.toLocaleDateString("en-US", options)
 
 
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,  newListItems: items
   })
 
 
 });
+
+app.post("/", function (req,res){
+let item = req.body.newItem;
+items.push(item);
+res.redirect("/");
+})
 
 app.listen(3000, function() {
   console.log("server started on port3000");
